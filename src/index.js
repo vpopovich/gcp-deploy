8271c6a56d016444bed39744e9eb950856d6b6a2
Vladimir Popovich <vladimir.popovich@exyte.com>
2021-01-05 01:39:15 +0700

    Add github actions

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+function requestHandler(req, res) {
+    res.write('ok');
+    res.end();
+}
+const server = http.createServer(requestHandler);
+const port = process.env.PORT || 8888;
+server.listen(port, () => console.log(`listening on port ${port}`));
